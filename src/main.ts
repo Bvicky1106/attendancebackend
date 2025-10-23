c63a9f5cb15b2033112dc1dd1af89216d33bf832
VIGNESH B <117636277+Bvicky1106@users.noreply.github.com>
2025-10-23 14:45:15 +0530

    neew changes

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for multiple origins
   app.enableCors({
-    origin: 'http://localhost:3000', // Replace with your React port
+    origin: [
+      'http://localhost:3000', // local React dev
+      'https://ascentware-attend.vercel.app', // hosted React
+    ],
+    credentials: true, // if you want cookies or auth headers
   });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(8000);
 }
